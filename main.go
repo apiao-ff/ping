e45bc7a1f078d6d648ecd5c1029eb9aeace02ed1
apiao-ff <1813709209@qq.com>
2025-08-08 10:43:49 +0800

    修改main

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong pong pong",
+			"message": "hello world!",
 		})
 	})
 	r.Run(":8080")
